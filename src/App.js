e038deaaa1850ee22f201ab8710a6ce4067b6fcf
mockey-jockey <mani.tamilarasan@freshworks.com>
2020-02-10 15:16:53 +0530

    removed hybrid

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Edit src/App.js and save to reload.
         </p>
         <a
           className="App-link"
